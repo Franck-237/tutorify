4a9a8197115b66d5c5289f0e093666fc034bca9e
franck-237 <franckkamdem260104@gmail.com>
2024-03-11 14:33:58 +0100

    react-router

---
+++ tutorify/src/components/Header.jsx
@@ -0,0 +1,58 @@
+import React from 'react';
+
+import line from '../assets/img/Line 2.png';
+import boy from '../assets/img/boy.png';
+import japan from '../assets/img/japan.png';
+import turkisk from '../assets/img/turkish.png';
+import italia from '../assets/img/italia.png';
+import group from '../assets/img/Group 3.png';
+import chart from '../assets/img/Chart.png';
+
+
+const Header = () => {
+  return (
+    <div className='container mx-auto px-5 pt-2'>
+        <div className='flex flex-col md:flex-row justify-between items-center'>
+            <div className='md:w-1/2'>
+                <div className='flex justify-start items-center space-x-4 text-[#FB9C46] font-semibold text-[16px] pb-4'>
+                    <p>We are The Best </p>
+                    <span><img src={line} alt="" /></span>
+                </div>
+                <div className='pb-4'>
+                    <p className='text-[32px] font-bold'>Learn Frome Home With <span className='text-[#FB9C46]'>The Best</span> Online Languange Tutors</p>
+                </div>
+                <div className='pb-4'>
+                    <p className='text-[#90A3B4] font-medium'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Id interdum dui mollis . Suspendisse nulla :</p>
+                </div>
+                <div className='pb-8'>
+                    <button className='text-white bg-[#058E6E] py-2 px-4 border rounded-xl shadow-md'>Try Free Lessons</button>
+                </div>
+            </div>
+            <div>
+                <div className='absolute flex justify-between items-center w-[250px] h-[250px] gap-[80px]'>
+                    <div>
+                        <img src={japan} alt="" className='pt-20 pr-10 w-[500px]'/>
+                    </div>
+                    <div>
+                        <img src={turkisk} alt="" className='w-[300px]'/>
+                    </div>
+                </div>
+                <div className='absolute flex justify-between items-center w-[250px] h-[250px] gap-[80px] pt-[250px] md:pt-[350px]'>
+                    <div className='pr-10'>
+                        <img src={chart} alt=""  className='absolute pt-12 pr-10'/>
+                        <img src={group} alt="" className='w-[500px] bg-white border rounded-xl py-3 px-4 text-center mt-20 mr-10'/>
+                    </div>
+                    <div>
+                        <img src={italia} alt="" className='w-[500px]'/>
+                    </div>
+                </div>
+                <div className='w-[219px] h-[296px] md:w-[300px] md:h-[350px] bg-[#FB9C46] py-3 px-6 md:py-10 md:px-12 border rounded-full mt-20'>
+                    <img src={boy} alt="" className='pt-2'/>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+};
+
+export default Header;
---
+++ tutorify/src/components/Navbar.jsx
@@ -0,0 +1,59 @@
+import React, { useState } from 'react';
+
+import { FiMenu } from "react-icons/fi";
+import { MdClose } from "react-icons/md";
+
+const Navbar = () => {
+
+    const Links = [
+        {
+            title: 'Home',
+        },{
+            title: 'Lessons'
+        },{
+            title: 'Find Tutors'
+        },{
+            title: 'About Us'
+        },
+    ];
+
+    const [nav, setNav] = useState(false);
+
+  return (
+    <div className='container mx-auto px-5 pt-5'>
+        <div className='flex justify-between items-center pb-3 md:pb-5'>
+            <div>
+                <p className='text-[poppins] text-[#393939] text-[18px] font-bold'>Tutorify</p>
+            </div>
+            <div className='overflow-hidden'>
+                <div onClick={() => setNav(!nav)} className='md:hidden'>
+                        {
+                            nav ?
+                            <MdClose className='text-[#FB9C46] w-[24px] h-[29.58px] md:hidden'/>
+                             : 
+                            <FiMenu className='text-[#FB9C46] w-[24px] h-[29.58px] md:hidden'/>
+                        }
+                </div>
+             <div className={`absolute bg-gray-200 pt-10 flex h-[90vh] flex-col md:pt-0 md:bg-white md:relative left-0 w-[100%] md:w-auto md:h-auto md:flex-row justify-start md:justify-between items-center space-y-5 md:space-y-0 md:space-x-10 text-[16px] font-semibold ${nav ? 'left-0 transition-all duration-700' : 'left-[-1000px] transition-all duration-700'} md:left-0`}>
+                <ul className=''>
+                    {
+                        Links.map((data) => (
+                            <li key={data.title} className='space-y-4 md:space-y-0 md:inline-block md:pr-4'>
+                                <a href="" className='text-[#393939] hover:text-[#FB9C46] focus:text-[#FB9C46]'>{data.title}</a>
+                            </li>
+                        ))
+                    }
+                </ul>
+                <div className='flex flex-col space-y-5 md:flex-row md:space-y-0 md:space-x-4 text-white'>
+                    <button className='bg-[#407F55] px-5 py-1 border rounded-lg'>Login</button> 
+                    <button className='bg-[#FB9C46] px-5 py-1 border rounded-lg'>Sign Up</button>
+                </div>
+             </div>
+            </div>
+        </div>
+        <hr className='text-[#C4C4C4]'/>
+    </div>
+  )
+};
+
+export default Navbar;
